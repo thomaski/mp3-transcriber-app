5445bf922e781696ce5e2201eca5fe085ae49989
tom <thomas.kiesswetter@gmx.de>
2026-02-18 23:29:47 +0100

    Security: Passwort-Placeholder entfernt (kein Hinweis auf Vorname)

--- client/src/components/public/PublicLandingPage.js
+++ client/src/components/public/PublicLandingPage.js
@@ -176,7 +176,7 @@ function PublicLandingPage() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition"
-              placeholder="Vorname eingeben"
+              placeholder=""
               disabled={verifying}
               autoFocus
               autoComplete="off"
